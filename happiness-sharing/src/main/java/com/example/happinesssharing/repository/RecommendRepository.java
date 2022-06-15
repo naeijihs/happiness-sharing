c7958c37b45a708634ce4fe2e08e57db0ef654b0
shijiean <1592366283@qq.com>
2022-06-15 10:48:51 +0800

    complete

--- happiness-sharing/src/main/java/com/example/happinesssharing/repository/RecommendRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.happinesssharing.repository;
-
-import com.example.happinesssharing.entity.Recommend;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecommendRepository extends BaseRepository<Recommend,Integer> {
-
-}
